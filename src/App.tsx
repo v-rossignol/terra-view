a81b58538782e57c62f95a7cb20d7e9576a7b738
Vincent ROSSIGNOL <rossignol.vincent@gmail.com>
2026-06-13 14:11:45 +0200

    Add App.tsx root component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Terra View</h1>
+      <p>2D planetary surface visualization client for Infinity.</p>
+    </div>
+  )
+}
+
+export default App
