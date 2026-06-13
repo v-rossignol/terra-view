2ee03db02670a2741c287bcabebedcfef22d0221
Vincent ROSSIGNOL <rossignol.vincent@gmail.com>
2026-06-13 15:05:01 +0200

    initializing project

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/terra-view/',
   plugins: [react()],
+  server: {
+    port: 3000,
+    allowedHosts: ['infinity-dev.home.rh', 'localhost'],
+    origin: 'http://infinity-dev.home.rh',
+    hmr: {
+      host: 'infinity-dev.home.rh',
+      clientPort: 80,
+    },
+    proxy: {
+      '/infinity': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
