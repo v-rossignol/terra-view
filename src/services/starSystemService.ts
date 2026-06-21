418609cd5971d30d6d7064d5d6079f653a112de0
Vincent ROSSIGNOL <rossignol.vincent@gmail.com>
2026-06-21 16:15:55 +0200

    Update starSystemService.ts

--- src/services/starSystemService.ts
+++ src/services/starSystemService.ts
@@ -3,7 +3,7 @@ import { api } from './api';
 
 export const starSystemService = {
   async getStarSystem(systemId: string): Promise<StarSystem> {
-    const response = await api.get<StarSystem>(`/galaxy/systems/${systemId}`);
+    const response = await api.get<StarSystem>(`/systems/${systemId}`);
     return response.data;
   },
 };
